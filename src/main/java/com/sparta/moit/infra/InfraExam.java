2bf3fc6ac55e4e5ae8aadd1e9e2a9c7dd6df91fb
Song-dyong <brandy1930@nate.com>
2024-03-29 13:46:32 +0900

    Test: Jira Done Test #DONE MOITBE-29

--- src/main/java/com/sparta/moit/infra/InfraExam.java
+++ src/main/java/com/sparta/moit/infra/InfraExam.java
@@ -1,11 +1,5 @@
 package com.sparta.moit.infra;
 
 public class InfraExam {
-    // test
-    // done test
-    // #DONE test
-    // #DONE test UpperCase
-    // Jira Add Trigger
-    // Jira Add Trigger #done
-    // DONE first
+
 }
