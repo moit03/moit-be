5d9ec4c0f89e851abc4e4255c4267029a1c72db3
Song-dyong <brandy1930@nate.com>
2024-03-30 11:03:26 +0900

    Test: GithubAction CI/CD test

--- src/main/java/com/sparta/moit/global/controller/CommonController.java
+++ src/main/java/com/sparta/moit/global/controller/CommonController.java
@@ -1,7 +1,13 @@
 package com.sparta.moit.global.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CommonController {
+    @GetMapping("/")
+    public ResponseEntity<?> healthyCheck(){
+        return ResponseEntity.ok().body("Connection OK");
+    }
 }
