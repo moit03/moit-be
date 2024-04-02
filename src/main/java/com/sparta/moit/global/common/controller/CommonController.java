2091d6e1acc50443a18e95d42202c4054a0d3918
Song-dyong <brandy1930@nate.com>
2024-04-03 02:28:01 +0900

    fix: cors 디버깅

--- src/main/java/com/sparta/moit/global/common/controller/CommonController.java
+++ src/main/java/com/sparta/moit/global/common/controller/CommonController.java
@@ -2,15 +2,23 @@ package com.sparta.moit.global.common.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j(topic = "CommonController")
 @RestController
+@CrossOrigin(origins = "*")
 public class CommonController {
     @GetMapping("/")
     public ResponseEntity<?> healthCheck(){
         log.info("Connection OK");
         return ResponseEntity.ok().body("Connection OK");
+    }
+
+    @GetMapping("/my-endpoint")
+    public String myEndpoint() {
+        log.debug("CORS 요청 확인");
+        return "Hello";
     }
 }
