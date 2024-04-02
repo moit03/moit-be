3317c293acd0fa16a8e683b1f6cb467ac5cfed4d
Song-dyong <brandy1930@nate.com>
2024-04-03 01:40:14 +0900

    fix: CORS HOTFIX 9

--- src/main/java/com/sparta/moit/global/common/controller/CommonController.java
+++ src/main/java/com/sparta/moit/global/common/controller/CommonController.java
@@ -1,13 +1,16 @@
 package com.sparta.moit.global.common.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j(topic = "CommonController")
 @RestController
 public class CommonController {
     @GetMapping("/")
     public ResponseEntity<?> healthCheck(){
+        log.info("Connection OK");
         return ResponseEntity.ok().body("Connection OK");
     }
 }
