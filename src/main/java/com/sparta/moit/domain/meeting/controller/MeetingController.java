bc4a2df62cbda9f3097dbba003918b1d9d6af5fe
Song-dyong <brandy1930@nate.com>
2024-04-05 02:20:32 +0900

    [Feat] 기술스택, 경력 필터 구현

--- src/main/java/com/sparta/moit/domain/meeting/controller/MeetingController.java
+++ src/main/java/com/sparta/moit/domain/meeting/controller/MeetingController.java
@@ -9,7 +9,6 @@ import com.sparta.moit.global.common.dto.ResponseDto;
 import com.sparta.moit.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
