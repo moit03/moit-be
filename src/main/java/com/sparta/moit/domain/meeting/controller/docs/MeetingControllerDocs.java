48a70dd66f335dc315ab7697d3082df65016dad8
Song-dyong <brandy1930@nate.com>
2024-04-04 22:59:02 +0900

    [Fix] Swagger Docs 추가

--- src/main/java/com/sparta/moit/domain/meeting/controller/docs/MeetingControllerDocs.java
+++ src/main/java/com/sparta/moit/domain/meeting/controller/docs/MeetingControllerDocs.java
@@ -1,9 +1,7 @@
 package com.sparta.moit.domain.meeting.controller.docs;
 
 import com.sparta.moit.domain.meeting.dto.CreateMeetingRequestDto;
-import com.sparta.moit.domain.meeting.dto.GetMeetingResponseDto;
 import com.sparta.moit.domain.meeting.dto.UpdateMeetingRequestDto;
-import com.sparta.moit.domain.member.entity.Member;
 import com.sparta.moit.global.security.UserDetailsImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -15,12 +13,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@Tag(name = "미팅", description = "미팅 API")
+@Tag(name = "모임", description = "모임 API")
 public interface MeetingControllerDocs {
 
-    @Operation(summary = "미팅 등록", description = "미팅 등록 API")
+    @Operation(summary = "모임 등록", description = "모임 등록 API")
     ResponseEntity<?> createMeeting(@RequestBody CreateMeetingRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails);
 
-    @Operation(summary = "미팅 수정", description = "미팅 수정 API")
+    @Operation(summary = "모임 수정", description = "모임 수정 API")
     ResponseEntity<?> updateMeeting(@PathVariable Long meetingId, @RequestBody UpdateMeetingRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails);
+
+    @Operation(summary = "모임 조회", description = "모임 조회 API")
+    ResponseEntity<?> getMeetingListByLatLng(@RequestParam Double locationLat,
+                                             @RequestParam Double locationLng,
+                                             @RequestParam(required = false) List<Short> skillId,
+                                             @RequestParam(required = false) List<Short> careerId,
+                                             @RequestParam(defaultValue = "1") int page);
 }
