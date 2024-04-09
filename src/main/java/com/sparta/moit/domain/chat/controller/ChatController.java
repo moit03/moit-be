26b1bc630ec51af59ab9dcee41c9d1b7b5a05570
Song-dyong <brandy1930@nate.com>
2024-04-09 10:29:23 +0900

    [Feat] WebSocket interceptor 추가

--- src/main/java/com/sparta/moit/domain/chat/controller/ChatController.java
+++ src/main/java/com/sparta/moit/domain/chat/controller/ChatController.java
@@ -4,7 +4,6 @@ import com.sparta.moit.domain.chat.dto.SendChatRequestDto;
 import com.sparta.moit.domain.chat.dto.ChatResponseDto;
 import com.sparta.moit.domain.chat.service.ChatService;
 import com.sparta.moit.domain.member.entity.Member;
-import com.sparta.moit.domain.member.entity.UserRoleEnum;
 import com.sparta.moit.global.common.dto.ResponseDto;
 import com.sparta.moit.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +13,6 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j(topic = "채팅 Controller")
@@ -26,24 +24,18 @@ public class ChatController {
     private final SimpMessagingTemplate messagingTemplate;
 
     /* 채팅방 입장 전 채팅 목록 불러오기 */
-    @GetMapping("/{meetingId}/chats")
+    @GetMapping("/api/meetings/{meetingId}/chats")
     public ResponseEntity<?> getChatList(@PathVariable Long meetingId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        log.info("채팅방 입장 meeting id " + meetingId.toString());
-        log.info("로그인 유저 이메일 " + userDetails.getUser().getEmail());
         ChatResponseDto responseDto = chatService.getChatList(userDetails.getUser(), meetingId);
         return ResponseEntity.ok().body(ResponseDto.success("채팅 불러오기 완료", responseDto));
     }
     /* 채팅 보내기, 구독하기 */
-    @Transactional
     @MessageMapping("/api/meetings/{meetingId}/chat/{memberId}")
     public void sendChat(@DestinationVariable Long meetingId
-//            , @AuthenticationPrincipal UserDetailsImpl userDetails
+            , @AuthenticationPrincipal UserDetailsImpl userDetails
             , @RequestBody SendChatRequestDto sendChatRequestDto) {
-        log.info("온 메시지 " + sendChatRequestDto.getContent());
-        log.info("미팅 id " + meetingId.toString());
-//        log.info("로그인 유저 이메일 " + userDetails.getUser().getEmail());
-//        Member member = userDetails.getUser();
-        messagingTemplate.convertAndSend("/topic/rooms/" + meetingId + "/chat", chatService.sendChat(meetingId, sendChatRequestDto));
+        Member member = userDetails.getUser();
+        messagingTemplate.convertAndSend("/topic/rooms/" + meetingId + "/chat", chatService.sendChat(meetingId, member, sendChatRequestDto));
     }
 
 }
--- src/main/java/com/sparta/moit/global/config/StompHandler.java
+++ src/main/java/com/sparta/moit/global/config/StompHandler.java
@@ -1,39 +1,69 @@
-//package com.sparta.moit.global.config;
-//
-//import com.sparta.moit.global.jwt.JwtUtil;
-//import jakarta.servlet.http.HttpServletResponse;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.messaging.Message;
-//import org.springframework.messaging.MessageChannel;
-//import org.springframework.messaging.simp.stomp.StompCommand;
-//import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-//import org.springframework.messaging.support.ChannelInterceptor;
-//import org.springframework.security.access.AccessDeniedException;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.stereotype.Component;
-//
-//import java.io.IOException;
-//import java.util.Objects;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class StompHandler implements ChannelInterceptor {
-//    private final JwtUtil jwtUtil;
-//
-//    @Override
-//    public Message<?> preSend(Message<?> message, MessageChannel channel) {
-//        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
-//        System.out.println("message:" + message);
-//        System.out.println("헤더 : " + message.getHeaders());
-//        System.out.println("토큰" + accessor.getNativeHeader("Authorization"));
-//        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
-//            try {
-//                jwtUtil.validateToken(Objects.requireNonNull(accessor.getFirstNativeHeader("Authorization")).substring(7));
-//            } catch (IOException e) {
-//                throw new RuntimeException(e);
-//            }
-//        }
-//        return message;
-//    }
-//}
+package com.sparta.moit.global.config;
+
+import com.sparta.moit.domain.member.entity.Member;
+import com.sparta.moit.domain.member.repository.MemberRepository;
+import com.sparta.moit.global.error.CustomException;
+import com.sparta.moit.global.error.ErrorCode;
+import com.sparta.moit.global.jwt.JwtUtil;
+import com.sparta.moit.global.security.UserDetailsImpl;
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j(topic = "StompHandler log")
+@Component
+@RequiredArgsConstructor
+public class StompHandler implements ChannelInterceptor {
+    private final JwtUtil jwtUtil;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        // 연결 요청시 JWT 검증
+        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+            // Authorization 헤더 추출
+            List<String> authorization = accessor.getNativeHeader("Authorization");
+            if (authorization != null && !authorization.isEmpty()) {
+                String jwt = authorization.get(0).substring(7);
+                try {
+                    // JWT 토큰 검증
+                    Claims decodedJWT = jwtUtil.getUserInfoFromToken(jwt);
+                    String email = decodedJWT.getSubject();
+                    // 사용자 정보 조회
+                    Member member = memberRepository.findByEmail(email).orElseThrow(() ->
+                            new CustomException(ErrorCode.NOT_EXIST_USER)
+                    );
+
+                    // 사용자 인증 정보 설정
+                    UserDetailsImpl userDetails = new UserDetailsImpl(member);
+                    UsernamePasswordAuthenticationToken authentication =
+                            new UsernamePasswordAuthenticationToken(
+                                    userDetails, null, userDetails.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                } catch (RuntimeException e) {
+                    log.error("JWT Verification Failed: " + e.getMessage());
+                    return null;
+                } catch (Exception e) {
+                    log.error("An unexpected error occurred: " + e.getMessage());
+                    return null;
+                }
+            } else {
+                // 클라이언트 측 타임아웃 처리
+                log.error("Authorization header is not found");
+                return null;
+            }
+        }
+        return message;
+    }
+}
