60a9d5e2e41b687fcb8a8425fcbb530cfaba30b2
OKSUchun <oksuchun119@gmail.com>
2024-04-09 16:16:49 +0900

    [Fix] StompHandler Auth 수정 - ChatController: @AuthenticationPrincipal -> Principal - ChatService : User -> email 사용으로 변경 - StompHandler : StompHeaderAccessor.getAccessor(message, StompHeaderAccessor.class) , accessor.setUser(authentication) 으로 수정 - UserDetailsImpl : 'User' -> authority로 수정

--- src/main/java/com/sparta/moit/global/config/StompHandler.java
+++ src/main/java/com/sparta/moit/global/config/StompHandler.java
@@ -14,6 +14,7 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -29,7 +30,9 @@ public class StompHandler implements ChannelInterceptor {
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
-        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+//        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        StompHeaderAccessor accessor = MessageHeaderAccessor
+                .getAccessor(message, StompHeaderAccessor.class);
         // 연결 요청시 JWT 검증
         if (StompCommand.CONNECT.equals(accessor.getCommand())) {
             // Authorization 헤더 추출
@@ -51,6 +54,8 @@ public class StompHandler implements ChannelInterceptor {
                             new UsernamePasswordAuthenticationToken(
                                     userDetails, null, userDetails.getAuthorities());
                     SecurityContextHolder.getContext().setAuthentication(authentication);
+                    // accessor에 등록
+                    accessor.setUser(authentication);
                 } catch (RuntimeException e) {
                     log.error("JWT Verification Failed: " + e.getMessage());
                     return null;
