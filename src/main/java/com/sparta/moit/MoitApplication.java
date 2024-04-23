d12b4e99f9745341b4a37985a91742a27fac8d2c
Song-dyong <brandy1930@nate.com>
2024-04-23 17:00:59 +0900

    [Fix] TimeZone UTC -> Asia/Seoul

--- src/main/java/com/sparta/moit/MoitApplication.java
+++ src/main/java/com/sparta/moit/MoitApplication.java
@@ -2,16 +2,25 @@ package com.sparta.moit;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @OpenAPIDefinition(servers = {@Server(url = "https://hhboard.xyz", description = "Default Server URL")})
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableScheduling
 public class MoitApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(MoitApplication.class, args);
     }
