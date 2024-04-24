6f2e640855614ddd4f38116b439038727a09d6de
OKSUchun <oksuchun119@gmail.com>
2024-04-24 09:45:55 +0900

    [Chore] Paginator util - 공용 paginator util

---
+++ src/main/java/com/sparta/moit/global/util/pagination/ListPaginator.java
@@ -0,0 +1,10 @@
+package com.sparta.moit.global.util.pagination;
+
+import java.util.List;
+
+public class ListPaginator<T> implements Paginator<T>{
+    @Override
+    public boolean hasNextPage(List<T> list, int pageSize) {
+        return false;
+    }
+}
