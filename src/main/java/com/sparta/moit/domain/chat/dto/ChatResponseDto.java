194e5bc858f1785dc1994dcb91ded0e6d03dba8c
Song-dyong <brandy1930@nate.com>
2024-04-25 20:49:18 +0900

    [Fix] ChatResponseDto Status 추가

--- src/main/java/com/sparta/moit/domain/chat/dto/ChatResponseDto.java
+++ src/main/java/com/sparta/moit/domain/chat/dto/ChatResponseDto.java
@@ -1,6 +1,7 @@
 package com.sparta.moit.domain.chat.dto;
 
 import com.sparta.moit.domain.chat.entity.Chat;
+import com.sparta.moit.domain.meeting.entity.MeetingStatusEnum;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.domain.Slice;
@@ -14,15 +15,18 @@ public class ChatResponseDto {
 
     private final Long meetingId;
 
+    private final MeetingStatusEnum meetingStatusEnum;
+
     private final Slice<SingleChatResponseDto> chats;
 
     @Builder
-    public ChatResponseDto(Long meetingId, Slice<SingleChatResponseDto> chats) {
+    public ChatResponseDto(Long meetingId, MeetingStatusEnum meetingStatusEnum, Slice<SingleChatResponseDto> chats) {
         this.meetingId = meetingId;
+        this.meetingStatusEnum = meetingStatusEnum;
         this.chats = chats;
     }
 
-    public static ChatResponseDto fromEntity(Slice<Chat> chatEntitySlice, Long meetingId) {
+    public static ChatResponseDto fromEntity(Slice<Chat> chatEntitySlice, Long meetingId, MeetingStatusEnum meetingStatusEnum) {
         List<SingleChatResponseDto> chatDtos = chatEntitySlice.getContent().stream()
                 .map(SingleChatResponseDto::fromEntity)
                 .collect(Collectors.toList());
@@ -31,6 +35,7 @@ public class ChatResponseDto {
 
         return ChatResponseDto.builder()
                 .meetingId(meetingId)
+                .meetingStatusEnum(meetingStatusEnum)
                 .chats(chatDtoSlice).build();
     }
 }
