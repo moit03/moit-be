41968509d5a1aadb9ff0fdec36d870ff58cdbc83
Song-dyong <brandy1930@nate.com>
2024-04-27 11:05:34 +0900

    [Test] Postgre 타임존 변경 + 각종 format 로그 추가

--- src/main/java/com/sparta/moit/domain/meeting/dto/GetMeetingDetailResponseDto.java
+++ src/main/java/com/sparta/moit/domain/meeting/dto/GetMeetingDetailResponseDto.java
@@ -3,16 +3,16 @@ package com.sparta.moit.domain.meeting.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.sparta.moit.domain.meeting.entity.Meeting;
 import com.sparta.moit.domain.meeting.entity.MeetingStatusEnum;
-import com.sparta.moit.domain.member.entity.Member;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
+@Slf4j(topic = "GetMeetingDetailResponseDto")
 @Getter
 public class GetMeetingDetailResponseDto {
 
@@ -24,10 +24,19 @@ public class GetMeetingDetailResponseDto {
     private final List<String> skillNameList;
     private final LocalDate meetingDate;
 
-    @JsonFormat(pattern = "HH:mm")
+    //    @JsonFormat(pattern = "HH:mm")
     private final LocalDateTime meetingStartTime;
-    @JsonFormat(pattern = "HH:mm")
+//    @JsonFormat(pattern = "HH:mm")
     private final LocalDateTime meetingEndTime;
+
+    @JsonFormat(pattern = "HH:mm", timezone = "GMT+9")
+    private final Date jsonFormatStartTime;
+
+    @JsonFormat(pattern = "HH:mm", timezone = "GMT+9")
+    private final LocalDateTime localJsonFormatStartTime;
+
+    @JsonFormat(pattern = "HH:mm", timezone = "GMT+9")
+    private final LocalDateTime utcFormatStartTime;
 
     private final String locationAddress;
 
@@ -42,7 +51,7 @@ public class GetMeetingDetailResponseDto {
     private final boolean isBookmarked;
 
     @Builder
-    public GetMeetingDetailResponseDto(Long meetingId, String meetingName, String creatorName, String creatorEmail, List<String> careerNameList, List<String> skillNameList, LocalDate meetingDate, LocalDateTime meetingStartTime, LocalDateTime meetingEndTime, String locationAddress, Short registeredCount, Short totalCount, Integer budget, String contents, Double locationLat, Double locationLng, boolean isJoin, MeetingStatusEnum status, boolean isBookmarked) {
+    public GetMeetingDetailResponseDto(Long meetingId, String meetingName, String creatorName, String creatorEmail, List<String> careerNameList, List<String> skillNameList, LocalDate meetingDate, LocalDateTime meetingStartTime, LocalDateTime meetingEndTime, Date jsonFormatStartTime, LocalDateTime localJsonFormatStartTime, LocalDateTime utcFormatStartTime, String locationAddress, Short registeredCount, Short totalCount, Integer budget, String contents, Double locationLat, Double locationLng, boolean isJoin, MeetingStatusEnum status, boolean isBookmarked) {
         this.meetingId = meetingId;
         this.meetingName = meetingName;
         this.creatorName = creatorName;
@@ -52,6 +61,9 @@ public class GetMeetingDetailResponseDto {
         this.meetingDate = meetingDate;
         this.meetingStartTime = meetingStartTime;
         this.meetingEndTime = meetingEndTime;
+        this.jsonFormatStartTime = jsonFormatStartTime;
+        this.localJsonFormatStartTime = localJsonFormatStartTime;
+        this.utcFormatStartTime = utcFormatStartTime;
         this.locationAddress = locationAddress;
         this.registeredCount = registeredCount;
         this.totalCount = totalCount;
@@ -73,6 +85,11 @@ public class GetMeetingDetailResponseDto {
                 .map(SkillResponseDto::getSkillName)
                 .collect(Collectors.toList());
 
+
+        Date jsonFormatStartTime = Date.from(meeting.getMeetingStartTime().atZone(ZoneId.systemDefault()).toInstant());
+        log.info("meeting.getMeetingStartTime() = " + meeting.getMeetingStartTime().toString());
+        log.info("meeting.getMeetingEndTime() = " + meeting.getMeetingEndTime().toString());
+
         return GetMeetingDetailResponseDto.builder()
                 .meetingId(meeting.getId())
                 .meetingName(meeting.getMeetingName())
@@ -83,6 +100,9 @@ public class GetMeetingDetailResponseDto {
                 .meetingDate(meeting.getMeetingDate())
                 .meetingStartTime(meeting.getMeetingStartTime())
                 .meetingEndTime(meeting.getMeetingEndTime())
+                .jsonFormatStartTime(jsonFormatStartTime)
+                .localJsonFormatStartTime(meeting.getMeetingStartTime())
+                .utcFormatStartTime(meeting.getMeetingStartTime())
                 .locationAddress(meeting.getLocationAddress())
                 .registeredCount(meeting.getRegisteredCount())
                 .totalCount(meeting.getTotalCount())
