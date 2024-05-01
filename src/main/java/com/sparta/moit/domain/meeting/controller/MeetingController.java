3fea09b21b33ed458cb783b31072aa6a53dc8ff4
OKSUchun <oksuchun119@gmail.com>
2024-05-01 14:49:48 +0900

    [Fix] add import statement

--- src/main/java/com/sparta/moit/domain/meeting/controller/MeetingController.java
+++ src/main/java/com/sparta/moit/domain/meeting/controller/MeetingController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
