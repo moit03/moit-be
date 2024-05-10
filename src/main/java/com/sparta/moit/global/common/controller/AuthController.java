68559763217a301ba4b08bc426d6f106629379ed
OKSUchun <oksuchun119@gmail.com>
2024-05-11 01:46:47 +0900

    [Fix] Test refresh access token with Cookie

--- src/main/java/com/sparta/moit/global/common/controller/AuthController.java
+++ src/main/java/com/sparta/moit/global/common/controller/AuthController.java
@@ -1,30 +1,55 @@
 package com.sparta.moit.global.common.controller;
 
-import com.sparta.moit.domain.member.service.KakaoService;
 import com.sparta.moit.global.common.controller.docs.AuthControllerDocs;
 import com.sparta.moit.global.common.dto.RefreshTokenRequest;
 import com.sparta.moit.global.common.dto.ResponseDto;
 import com.sparta.moit.global.common.service.RefreshTokenService;
+import com.sparta.moit.global.jwt.JwtUtil;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @RestController
 @RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController implements AuthControllerDocs {
     private final RefreshTokenService refreshTokenService;
-    private final KakaoService kakaoService;
+    private final JwtUtil jwtUtil;
 
-    public AuthController(RefreshTokenService refreshTokenService, KakaoService kakaoService) {
-        this.refreshTokenService = refreshTokenService;
-        this.kakaoService = kakaoService;
+    @GetMapping("/refresh/test")
+    public ResponseEntity<?> refreshAccessTokenTest(HttpServletRequest request, HttpServletResponse response) {
+        /*리프레시 토큰이 없으면 badRequest 반환*/
+        String refreshToken = jwtUtil.getRefreshTokenFromCookie(request);
+
+        if (refreshToken == null || refreshToken.isEmpty()) {
+            return ResponseEntity.badRequest().body("토큰이 입력되지 않았습니다.");
+        }
+
+        /*리프레시 토큰 검증*/
+        boolean isValidRefreshToken = refreshTokenService.validateRefreshToken(refreshToken);
+        if (!isValidRefreshToken) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("인증이 필요합니다.");
+        }
+        /*리프레시 토큰으로 새로운 엑세스 토큰 발급*/
+        Optional<String> newAccessToken = refreshTokenService.refreshAccessToken(refreshToken);
+
+        /*accessToken, refreshToken cookie 추가*/
+        if (newAccessToken.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseDto.fail("엑세스 토큰 발급에 실패했습니다.", null));
+        } else {
+            jwtUtil.addAccessTokenToCookie(String.valueOf(newAccessToken), response);
+            jwtUtil.addRefreshTokenToCookie(refreshToken, response);
+        }
+        return  ResponseEntity.ok().body(ResponseDto.success("액세스 토큰이 발급되었습니다.", newAccessToken));
     }
 
     /*엑세스 토큰 갱신 API 호출*/
